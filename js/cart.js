0943ba8c2150d727ad0d506b4ac6cf04b02c5bc4
KhpAgency <108800686+KhpAgency@users.noreply.github.com>
2023-07-03 17:35:53 +0300

    jj

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,6 @@
+
 let baseUrl = "https://peacock-api-ixpn.onrender.com/api/v1/cart";
+// let productURL = "https://peacock-api-ixpn.onrender.com/api/v1/cart/${}";
 
 
 function getitems() {
@@ -13,8 +15,8 @@ function getitems() {
 
     axios.request(options)
         .then(function (response) {
-            console.log(response);
             const cart = response.data.data.cartItems
+            console.log(response.data.data);
             let data = cart.map((item) =>
                 // console.log(response.data.data)
                 `<div class="row mb-5" id="product">
@@ -22,7 +24,7 @@ function getitems() {
                 <div class="bg-image hover-overlay hover-zoom ripple rounded"
                     data-mdb-ripple-color="light">
                     <img src="${item.productID.images[0]}"
-                        class="w-100" alt="" />
+                        class="w-100 imgh" alt="" />
                     <a href="#!">
                         <div class="mask"
                             style="background-color: rgba(251, 251, 251, 0.2)"></div>
@@ -31,6 +33,7 @@ function getitems() {
             </div>
 
             <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                <input id="productid" value="${item._id}" type="text" >
                 <p><strong>${item.productID.title}</strong></p>
                 <p>${item.productID.description}</p>
                 ${item.productCategory == "ChocolateBox" ?
@@ -48,33 +51,35 @@ function getitems() {
                 </button>
             </div>
 
-            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0" id="changequantity">
                 <div class="d-flex mb-4 quantity">
-                    <button class="btn btn-primary px-3 me-2" onclick="decreaseQuantity()">
+                    <button class="btn btn-primary px-3 me-2" onclick="decreaseQuantity(${item.price},'${item._id}')">
                         <i class="fas fa-minus"></i>
                     </button>
 
                     <div class="form-outline">
-                        <input id="form1" min="0" name="quantity" value="${item.quantity}" type="number"
-                            class="form-control" />
+                        <input id="form1${item._id}" min="0" name="quantity" value=${item.quantity} type="number" disabled
+                            class="form-control active" />
                         <label class="form-label" for="form1">Quantity</label>
                     </div>
 
-                    <button class="btn btn-primary px-3 ms-2" onclick="increaseQuantity()">
+                    <button class="btn btn-primary px-3 ms-2" onclick="increaseQuantity(${item.price},'${item._id}')">
                         <i class="fas fa-plus"></i>
                     </button>
                 </div>
-                <p id="price" class="text-start text-md-center ml63">
-                    <strong>SAR&nbsp${item.price*item.quantity}</strong>
+                <p  class="text-start text-md-center ml63">
+                SAR&nbsp
+                <span id="price${item._id}" class="price-price">${item.price * item.quantity}</span>
+                    
                 </p>
             </div>
         </div>`).join("")
 
             document.getElementById("products-card").innerHTML = data;
 
-// -----------------summary--------------------------
-            const total=response.data.data.totalCartPrice;
-            let data2=`<ul class="list-group list-group-flush">
+            // -----------------summary--------------------------
+            const total = response.data.data.totalCartPrice;
+            let data2 = `<ul class="list-group list-group-flush">
             <li
                 class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
                 Products
@@ -97,22 +102,79 @@ function getitems() {
             </li>
         </ul>`
             document.getElementById("summary").innerHTML = data2;
-            document.getElementById("summarycol").style.display="block";
-            document.getElementById("cartcard").style.display="block";
-            
-            
-            
-        }).catch((err)=>{
+            document.getElementById("summarycol").style.display = "block";
+            document.getElementById("cartcard").style.display = "block";
+
+
+
+        }).catch((err) => {
             let element = `<P class="empty">Your cart is empty!</P>`
             document.getElementById("products-card").innerHTML = element;
-            document.getElementById("cartcard").style.display="block";
-            document.getElementById("cartcard").style.width="855px";
+            document.getElementById("cartcard").style.display = "block";
+            document.getElementById("cartcard").style.width = "855px";
         })
 
 
 
 }
 getitems();
+
+
+
+function increaseQuantity(price, id) {
+    let Qinput = document.getElementById("form1"+id)
+    let neww = parseInt(Qinput.value)
+    Qinput.value = neww + 1
+    updatePrice(Qinput.value, price ,id)
+
+    let productURL = `https://peacock-api-ixpn.onrender.com/api/v1/cart/${id}`;
+    const options = {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        },
+        url: productURL,
+        data: {quantity: Qinput.value}
+    };
+    axios.request(options).then((res)=>{
+        console.log(res.data.data.cartItems);
+        // location.reload()
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+
+}
+
+function decreaseQuantity(price, id) {
+    let Qinput = document.getElementById("form1"+id)
+    let neww = parseInt(Qinput.value)
+    Qinput.value = neww - 1
+
+    updatePrice(Qinput.value, price, id)
+
+        let productURL = `https://peacock-api-ixpn.onrender.com/api/v1/cart/${id}`;
+    const options = {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        },
+        url: productURL,
+        data: {quantity: Qinput.value}
+    };
+    axios.request(options).then((res)=>{
+        console.log(res.data.data.cartItems);
+        // location.reload()
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+function updatePrice(neww, price , id) {
+    var totalPrice = price * neww;
+    document.getElementById("price"+id).innerHTML = totalPrice
+    console.log(totalPrice);
+}
 
 // Check if quantity and total price exist in localStorage and update the input field and price
 // var quantityInput = document.getElementById("form1");
